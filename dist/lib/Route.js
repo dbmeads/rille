161b3d04e9d820183b03f3a76b5e98d48b70421d
David Meads <dbmeads@hotmail.com>
2016-04-30 19:36:14 -0400

    Refactor _route to use prototype.

--- dist/lib/Route.js
+++ dist/lib/Route.js
@@ -13,43 +13,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-function ensureGen(route, obj, key) {
-    return obj[key] ? obj[key] : obj[key] = _Route(route, key);
-}
-
-function nextGen(route, key) {
-    return key === '*' ? ensureGen(route, route, key) : ensureGen(route, route.children, key);
-}
-
-function child(route, keys) {
-    if (keys.length > 0) {
-        return child(nextGen(route, keys.shift()), keys);
-    }
-    return route;
-}
-
-function propagate(route, keys, entry) {
-    if (keys.length > 0) {
-        propagate(nextGen(route, keys.shift()), keys, entry);
-        propagate(nextGen(route, '*'), keys, entry);
-    } else {
-        route.subs.forEach(function (callback) {
-            return callback.apply(undefined, _toConsumableArray(entry));
-        });
-    }
-}
-
-function root(route) {
-    while (route.parent) {
-        route = route.parent;
-    }
-    return route;
-}
-
-function _push(route, entry) {
-    propagate(root(route), _Key2.default.parse(entry[0]), entry);
-}
-
 function _Route(parent, key) {
     var keys = parent ? parent.keys.map(function (v) {
         return v;
@@ -59,26 +22,53 @@ function _Route(parent, key) {
         keys.push(key);
     }
 
-    return {
+    Object.assign(this, {
         parent: parent,
         keys: keys,
         children: {},
         subs: new Set()
-    };
+    });
 }
+
+Object.assign(_Route.prototype, {
+    ensureGen: function ensureGen(obj, key) {
+        return obj[key] ? obj[key] : obj[key] = new _Route(this, key);
+    },
+    nextGen: function nextGen(key) {
+        return key === '*' ? this.ensureGen(this, key) : this.ensureGen(this.children, key);
+    },
+    child: function child(keys) {
+        if (keys.length > 0) {
+            return this.nextGen(keys.shift()).child(keys);
+        }
+        return this;
+    },
+    propagate: function propagate(keys, entry) {
+        if (keys.length > 0) {
+            this.nextGen(keys.shift()).propagate(keys, entry);
+            this.nextGen('*').propagate(keys, entry);
+        } else {
+            this.subs.forEach(function (callback) {
+                return callback.apply(undefined, _toConsumableArray(entry));
+            });
+        }
+    },
+    root: function root() {
+        return this.parent ? this.parent.root() : this;
+    },
+    push: function push(entry) {
+        this.root().propagate(this.keys, [_Key2.default.stringify(this.keys)].concat(entry));
+    }
+});
 
 function wrap(_route) {
     function route(key) {
-        return wrap(child(_route, _Key2.default.parse(key)));
+        return wrap(_route.child(_Key2.default.parse(key)));
     }
 
     return Object.assign(route, {
         push: function push() {
-            for (var _len = arguments.length, entry = Array(_len), _key = 0; _key < _len; _key++) {
-                entry[_key] = arguments[_key];
-            }
-
-            _push(_route, [_Key2.default.stringify(_route.keys)].concat(entry));
+            _route.push.apply(_route, arguments);
         },
         key: function key() {
             return _Key2.default.stringify(_route.keys);
@@ -93,7 +83,7 @@ function wrap(_route) {
 }
 
 function Route() {
-    return wrap(_Route());
+    return wrap(new _Route());
 }
 
 exports.default = Route;
