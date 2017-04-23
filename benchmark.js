95e76ef400fdb0868112678ad8731cca9f05adf7
David Meads <dbmeads@hotmail.com>
2017-04-23 19:15:26 -0400

    Add a no op

--- benchmark.js
+++ benchmark.js
@@ -10,6 +10,7 @@ rille('/*/*/*').sub(() => {});
 
 new Benchmark
     .Suite()
+    .add('no op', () => {})
     .add('pub (shallow route)', () => rille.pub())
     .add('pub (wildcard routes)', () => rille('/test/a/route').pub())
     .add('pub (deep route)', () => rille('/1/2/3/4/5/6/7/8/9/0').pub())
